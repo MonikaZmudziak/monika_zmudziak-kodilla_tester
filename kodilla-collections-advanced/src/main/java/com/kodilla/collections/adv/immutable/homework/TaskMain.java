631d2477c3774dff6b4cd8dfee81f01369ea3ad1
Monika Żmudziak <monika.zmudziak@gmail.com>
2023-10-08 14:45:53 +0200

    5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskMain {
+    public static void main(String[] args) {
+
+        Task task = new Task("Test login form", 6);
+
+        TaskRecord taskRecord = new TaskRecord("Test login form", 4);
+    }
+}
