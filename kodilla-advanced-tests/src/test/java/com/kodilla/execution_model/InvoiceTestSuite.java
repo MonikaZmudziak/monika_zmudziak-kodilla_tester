9a3b1c746f3a232979a73871745cd0f853efc489
Monika Żmudziak <monika.zmudziak@gmail.com>
2023-10-21 17:29:09 +0200

    Moduł 7.4

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/InvoiceTestSuite.java
@@ -0,0 +1,182 @@
+package com.kodilla.execution_model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class InvoiceTestSuite {
+
+    Invoice invoice = new Invoice();
+    Item milk = new Item("Milk", 3.50);
+    Item eggs = new Item("Eggs", 8.00);
+    Item bread = new Item("Bread", 4.15);
+
+    @Test
+    public void shouldAddItemsToInvoice() {
+        //When
+        int numberOfItems = invoice.getSize();
+
+        //Then
+        assertEquals(3, numberOfItems);
+    }
+
+    @Test
+    public void shouldGetExistingItem() {
+        //When
+        Item result = invoice.getItem(2);
+
+        //Then
+        assertEquals("Bread", result.getName());
+        assertEquals(4.15, result.getPrice(), 0.01);
+    }
+
+    @Test
+    public void shouldReturnNullWhenPassingNegativeIndex() {
+        //When
+        Item result = invoice.getItem(-3);
+
+        //Then
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldReturnNullWhenPassingOutOfRangeIndex() {
+        //When
+        Item result = invoice.getItem(7);
+
+        //Then
+        assertNull(result);
+    }
+
+    @Test
+    public void shouldClearInvoice() {
+        //When
+        invoice.clear();
+
+        //Then
+        assertEquals(0, invoice.getSize());
+    }
+
+    @BeforeEach // adnotacja wywołana przed uruchomieniem każdego testu
+    public void initializeInvoice() {
+        invoice.addItem(milk);
+        invoice.addItem(eggs);
+        invoice.addItem(bread);
+    }
+
+    @AfterEach // adnotacja wywołana po uruchomieniu każdego testu
+    public void resetValues() {
+        System.out.println("Resetting values...");
+    }
+
+    @BeforeAll // przed startem wszystkich testów
+    public static void displayIntroMessage() {
+        System.out.println("Starting testing");
+    }
+
+    @AfterAll // po zakończeniu wszystkich testów
+    public static void displayGoodByeMessage() {
+        System.out.println("Finishing testing");
+    }
+
+}
+
+//    @Test
+//    public void shouldAddItemsToInvoice() {
+//        //Given
+//        Invoice invoice = new Invoice();
+//        Item milk = new Item("Milk", 3.50);
+//        Item eggs = new Item("Eggs", 8.00);
+//        Item bread = new Item("Bread", 4.15);
+//        invoice.addItem(milk);
+//        invoice.addItem(eggs);
+//        invoice.addItem(bread);
+//
+//        //When
+//        int numberOfItems = invoice.getSize();
+//
+//        //Then
+//        assertEquals(3, numberOfItems);
+//    }
+//
+//    @Test
+//    public void shouldGetExistingItem() {
+//        //Given
+//        Invoice invoice = new Invoice();
+//        Item milk = new Item("Milk", 3.50);
+//        Item eggs = new Item("Eggs", 8.00);
+//        Item bread = new Item("Bread", 4.15);
+//        invoice.addItem(milk);
+//        invoice.addItem(eggs);
+//        invoice.addItem(bread);
+//
+//        //When
+//        Item result = invoice.getItem(2);
+//
+//        //Then
+//        assertEquals("Bread", result.getName());
+//        assertEquals(4.15, result.getPrice(), 0.01);
+//    }
+//
+//    @Test
+//    public void shouldReturnNullWhenPassingNegativeIndex() {
+//        //Given
+//        Invoice invoice = new Invoice();
+//        Item milk = new Item("Milk", 3.50);
+//        Item eggs = new Item("Eggs", 8.00);
+//        Item bread = new Item("Bread", 4.15);
+//        invoice.addItem(milk);
+//        invoice.addItem(eggs);
+//        invoice.addItem(bread);
+//
+//        //When
+//        Item result = invoice.getItem(-3);
+//
+//        //Then
+//        assertNull(result);
+//    }
+//
+//    @Test
+//    public void shouldReturnNullWhenPassingOutOfRangeIndex() {
+//        //Given
+//        Invoice invoice = new Invoice();
+//        Item milk = new Item("Milk", 3.50);
+//        Item eggs = new Item("Eggs", 8.00);
+//        Item bread = new Item("Bread", 4.15);
+//        invoice.addItem(milk);
+//        invoice.addItem(eggs);
+//        invoice.addItem(bread);
+//
+//        //When
+//        Item result = invoice.getItem(7);
+//
+//        //Then
+//        assertNull(result);
+//    }
+//
+//    @Test
+//    public void shouldClearInvoice() {
+//        //Given
+//        Invoice invoice = new Invoice();
+//        Item milk = new Item("Milk", 3.50);
+//        Item eggs = new Item("Eggs", 8.00);
+//        Item bread = new Item("Bread", 4.15);
+//        invoice.addItem(milk);
+//        invoice.addItem(eggs);
+//        invoice.addItem(bread);
+//        int invoiceSizeBeforeClear = invoice.getSize();
+//
+//        //When
+//        invoice.clear();
+//
+//        //Then
+//        assertEquals(0, invoice.getSize());
+//        assertEquals(3, invoiceSizeBeforeClear);
+//    }
+//
+//}
