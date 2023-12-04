23691228f09c83d06516af8d9d8d5aef44e9df05
Monika Żmudziak <monika.zmudziak@gmail.com>
2023-12-04 12:05:48 +0100

    Modul 13

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/form.java
@@ -0,0 +1,24 @@
+package com.kodilla.selenium;
+
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.support.ui.Select;
+//
+//public class form {
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\chrome\\chromedriver.exe");
+//        ChromeOptions.chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--remonte-allow-origins=*");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://kodilla.com/pl/test/form/");
+//
+//        WebElement yearCombo = driver.findElement(By.name("//*[@id=\"birthday-wrapper\"]/select[3]");
+//        Select yearSelect = new Select(yearCombo);
+//        yearSelect.selectByIndex(5);
+
+
+//    }
+//}
---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/login/LoginTestingApp.java
@@ -0,0 +1,24 @@
+//package com.kodilla.selenium.login;
+//
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//public class LoginTestingApp {
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\chrome\\chromedriver.exe");
+//        ChromeOptions.chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("--remonte-allow-origins=*");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://kodilla.com/pl/test/login/");
+//
+//        WebElement emailField = driver.findElement(By.xpath("//html/body/section/form/div[1]/input"));
+//        emailField.sendKeys("testuser@gmail.com");
+//        WebElement passwordField = driver.findElement(By.name("//html/body/section/form/div[2]/input"));
+//        emailField.sendKeys("Haslo");
+//
+//
+//    }
+//}
